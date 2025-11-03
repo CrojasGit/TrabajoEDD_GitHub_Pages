4ac34f86b2b4106b1d459bf9e0df3c06db60f49e
CrojasGit <crojasgo@alumnes.santjosepobrer.es>
2025-11-03 17:11:53 +0100

    Update noticiasIA.js
    agregada la parte de Mariana que consulta la fecha del momento

--- noticiasIA.js
+++ noticiasIA.js
@@ -6,6 +6,9 @@ const NEWS_API_URL = "https://newsapiproxy.carlosrojasgomariz.workers.dev/";
     let esAdmin = false;
     const TOTAL = 18;
     const VISIBLES = 5; // ahora 6 noticias visibles
+    // Fecha automática
+    document.getElementById('fecha').textContent =
+      new Date().toLocaleDateString('es-ES',{weekday:'long',day:'2-digit',month:'long',year:'numeric'});
 
     // === FRases del año (365) ===
     // Array de 365 frases únicas en español (una para cada día del año).
@@ -432,4 +435,5 @@ const NEWS_API_URL = "https://newsapiproxy.carlosrojasgomariz.workers.dev/";
     }
 
     // Inicia carga de noticias (asíncrona)
+
     cargarNoticias();
