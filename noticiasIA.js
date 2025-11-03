fba46ce702343a917e3203fbb3229cf4fa6b5335
CrojasGit <crojasgo@alumnes.santjosepobrer.es>
2025-11-03 19:35:02 +0100

    Update noticiasIA.js
    actu de la url cloudflare

--- noticiasIA.js
+++ noticiasIA.js
@@ -1,5 +1,5 @@
 const NEWS_API_URL = "https://newsapiproxy.carlosrojasgomariz.workers.dev/";
-    const GEMINI_API_URL = "https://gemini2.carlosrojasgomariz.workers.dev//";
+    const GEMINI_API_URL = "https://gemini2.carlosrojasgomariz.workers.dev/";
     const ADMIN_PASSWORD = "12345";
 
     let noticias = [];
@@ -436,6 +436,7 @@ const NEWS_API_URL = "https://newsapiproxy.carlosrojasgomariz.workers.dev/";
     // Inicia carga de noticias (asíncrona)
 
     cargarNoticias();
+
 
 
 
